a6183722a154e37c62cb6121d35038ba49ea5bb1
renaealisha54-debug <renaealisha54@gmail.com>
2026-04-23 15:20:47 -0400

    Create AppConstants.kt

---
+++ AppConstants.kt
@@ -0,0 +1,14 @@
+// File: src/main/java/com/yourpackage/vex/util/AppConfig.kt
+
+package com.yourpackage.vex.util
+
+object AppConfig {
+    const val APP_NAME = "Vex"
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "0.0.1-alpha"
+    
+    // Paths for your local toolchains
+    const val BASE_DIR = "/data/user/0/com.yourpackage.vex/"
+    const val EXTENSION_DIR = "${BASE_DIR}extensions/"
+    const val PROJECTS_DIR = "/sdcard/VexProjects/"
+}
